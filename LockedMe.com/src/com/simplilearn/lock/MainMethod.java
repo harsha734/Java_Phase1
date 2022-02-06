b7e42f2a5e2b9806ae2dbf43e70053eb290b3228
harsha <harsha478478@gmail.com>
2022-02-06 16:03:14 +0530

    Phase1 Assessment Project

---
+++ LockedMe.com/src/com/simplilearn/lock/MainMethod.java
@@ -0,0 +1,15 @@
+package com.simplilearn.lock;
+
+public class MainMethod {
+	
+	/*Enter your desired Directory path */
+	public static final String path = "E:\\Harsha"
+			+ ""; 
+	
+	public static void main(String[] args) {
+		MainMenu menu = new MainMenu();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+	
+}
